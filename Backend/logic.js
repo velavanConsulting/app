7f5d55cebc1cb88f764b7f5a38c1f0e00d82c9b0
velavanConsulting <consultingvelavan@gmail.com>
2025-09-06 06:53:03 +0530

    Update logic.js

--- Backend/logic.js
+++ Backend/logic.js
@@ -416,7 +416,7 @@ exports.sendWhatsAppNotification = (req, res) => {
 
   //const message = `Hello ${name},\n\nThis is a reminder that your vehicle ${vehicle} (${type}) is expiring on ${date}. Please renew it on time.\n\nThank you!\n- ${company}`;
   //const message = `வணக்கம் ${name},\n\nஉங்கள் வாகனம் ${vehicle} (${type}) ${date} அன்று காலாவதி ஆகிறது. தயவுசெய்து அதை நேரத்திற்கு முன்னே புதுப்பிக்கவும்.\n\nநன்றி!\n- ${company}`;
-  const message = `*ஸ்ரீ வேலவன் ஆட்டோ கன்சல்டிங்* \n\nவணக்கம் *${name}*,\n\nஉங்கள் வாகனம் *${vehicle}* (${type}) ${date} அன்று காலாவதி ஆகிறது. தயவுசெய்து அதை நேரத்திற்கு முன்னே புதுப்பிக்கவும்.\n\nநன்றி!`;
+  const message = `*ஸ்ரீ வேலவன் ஆட்டோ கன்சல்டிங்* \n\nவணக்கம் *${name}*,\n\nஉங்கள் வாகனம் *${vehicle} (${type}) ${date}* அன்று காலாவதி ஆகிறது. தயவுசெய்து அதை நேரத்திற்கு முன்னே புதுப்பிக்கவும்.\n\nநன்றி!`;
 	const whatsappLink = `https://wa.me/91${phone}?text=${encodeURIComponent(message)}`;
 
   res.redirect(whatsappLink);
@@ -881,6 +881,7 @@ function formatDateTime(date) {
     hour12: true
   }); // dd/mm/yyyy, hh:mm AM/PM
 };
+
 
 
 
